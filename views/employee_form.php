1a87eb48bfebcfaf32e4643d9430a8fc1b87e714
GueldenBayar <g.bayar@hotmail.de>
2025-11-25 15:36:05 +0100

    new employee_form file

--- views/employee_form.php
+++ views/employee_form.php
@@ -1,1 +1,31 @@
 <?php
+
+$fName = $emp ? $emp->getFirstName() : '';
+$lName = $emp ? $emp->getLastName() : '';
+$currentGender = $emp ? $emp->getGender() : '';
+$currentDeptId = $emp ? $emp->getDepartmentId() : 0;
+
+?>
+
+<h1><?= $headline ?></h1>
+
+<form action="<?= $formAction ?>">
+    First Name: <input type="text" name="firstName" value="<?= htmlspecialchars($fName) ?>" required><br>
+    Last Name: <input type="text" name="lastName" value="<?= htmlspecialchars($lName) ?>" required><br>
+    
+    Gender: <select name="gender">
+        <option value="<?= Gender::W->value ?>" <?= $currentGender === Gender::W->value ? 'selected' : ''?>>Female</option>
+        <option value="<?= Gender::M->value ?>" <?= $currentGender === Gender::M->value ? 'selected' : ''?>>Male</option>
+        <option value="<?= Gender::D->value ?>" <?= $currentGender === Gender::D->value ? 'selected' : ''?>>Divers</option>
+    </select><br>
+    
+    Abteilung: <select name="deptId">
+        <?php foreach ($allDepts as $d): ?>
+        <option value="<?= $d->getId() ?>" <?= $d->getId() === $currentDeptId ? 'selected' : '' ?>>
+            <?= htmlspecialchars($d->getName()) ?>
+        </option>
+        <?php endforeach ?>
+    </select><br><br>
+
+    <button type="submit" class="btn">Save</button>
+</form>
